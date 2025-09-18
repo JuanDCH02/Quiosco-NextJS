ed732bcdfaacd9af4dd7cdb6ffd5b9805682bef5
Juan David <juandclementeh@gmail.com>
2025-09-18 15:31:38 -0300

    boton confirmar orden

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -3,6 +3,7 @@ import { useStore } from "@/src/store"
 import ProductDetail from "./ProductDetail"
 import { useMemo } from "react"
 import { formatCurrency } from "@/src/utils"
+import { createOrder } from "@/actions/create-order-action"
 
 
 export default function OrderSummary() {
@@ -10,10 +11,15 @@ export default function OrderSummary() {
     const {order} = useStore()
     const total = useMemo(()=> order.reduce((total, item) => total + item.subtotal, 0), [order])
 
+    const handleCreateOrder= (formData: FormData)=> {
+        console.log(formData.get('name'))
+        createOrder()
+    }
+
     return (
         <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5 ">
             <h1 className="text-4xl text-center font-black">Mi pedido</h1>
-            {order.length===0? <p className="text-center my-10">carrito vacio</p> 
+            {order.length===0? <p className="text-center my-10">pedido vacio</p> 
                 : (
                     <div className="mt-5">
                         {order.map(item => (
@@ -26,8 +32,19 @@ export default function OrderSummary() {
                             total a pagar: 
                             <span className="font-bold"> {formatCurrency(total)}</span>
                         </p>
+                        <form className="space-y-5 w-full mt-10"
+                        action={handleCreateOrder}>
+                            <input type="text"
+                                placeholder="dinos tu nombre"
+                                className="bg-white border border-gray-200 p-2 w-full"
+                                name="name"
+                            />
+                            <input type="submit" value='confirmar pedido'
+                                className="py-2 rounded uppercase text-white
+                                 bg-black w-full text-center font-bold"
+                            />
+                        </form>
                     </div>
                 )}
         </aside>
-    )
-}
+)}
